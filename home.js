c322cb3137b23a94e5a9a282ba49e36279f713c5
Junwon Jang <junwonjang2018@u.northwestern.edu>
2017-11-04 02:26:39 -0400

    safety status toggle

--- home.js
+++ home.js
@@ -14,3 +14,23 @@ function sendSafetyRequest() {
   document.getElementById("modal_feedback").innerHTML = "Safety request has been sent.";
 }
 
+function changeTableSafety() {
+  var _safeToggle = document.getElementById("safe_toggle");
+  if (_safeToggle.checked) {
+    document.getElementById("my-safety-status").style.color = "#81C073";
+    document.getElementById("my-safety-status").style.backgroundColor = "white";
+    document.getElementById("my-safety-status").innerHTML = "SAFE";
+    document.getElementById("notify-family").innerHTML = "";
+  } else {
+    document.getElementById("my-safety-status").style.backgroundColor = "#D16969";
+    document.getElementById("my-safety-status").style.color = "white";
+    document.getElementById("my-safety-status").innerHTML = "SAFE?";
+    document.getElementById("notify-family").innerHTML = "Family has been notified.";
+  }
+}
+
+function init() {
+  console.log("hello");
+}
+
+init();
